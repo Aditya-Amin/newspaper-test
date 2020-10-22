c9c1eebe1067e58710db96b038b63ad089c36fdb
Aditya-Amin <adittyaamin@gmail.com>
2020-10-23 03:03:32 +0600

    PHP project initialized

---
+++ lib/Curl.php
@@ -0,0 +1,26 @@
+<?php
+
+
+/**
+ * Curl class for handling the API requests
+ * 
+ */
+
+
+ class Curl {
+
+    public static function get($url) {
+        $_init_ = curl_init();
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+        ];
+
+        curl_setopt_array($_init_, $options);
+        $results = curl_exec($_init_);
+        curl_close($_init_);
+
+        return json_decode($results, true);
+    }
+ }
---
+++ newspaper.php
@@ -0,0 +1,128 @@
+<?php
+require_once('lib/Curl.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- header start -->
+    <header>
+        <div id="header-warpper">
+            <div class="container">
+                <div class="wrapper">
+
+                    <div id="logo">
+                        <h2>Newspaper</h2>
+                    </div>
+                    
+                    <nav>
+                        <ul id="menu">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="newspaper.php">Newspaper</a></li>
+                            <li><a href="signup.php">Signup</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header end -->
+
+
+    <!-- banner start -->
+    <div id="banner-section">
+        <img src="assets/images/banner/newspaper.jpg" alt="" id="banner-image">
+    </div>
+    <!-- banner end -->
+
+
+    <!-- news start -->
+    <div class="section">
+        <div class="container">
+            <?php
+            $getAllPoliticsNews = Curl::get("https://content.guardianapis.com/search?q=politics&api-key=test");
+            $politics = $getAllPoliticsNews["response"]["results"];
+            ?>
+            <h1 class="section-title">Politics</h1>
+            <div class="wrapper flex-wrap">
+                <?php if(!empty($politics)): ?>
+                <?php foreach($politics as $item): ?>
+                <div class="news-item">
+                    <img src="assets/images/news/news.jpg" alt="">
+                    <h3><a target="_blank" href="<?php echo $item["webUrl"]; ?>"><?php echo $item["webTitle"]; ?></a></h3>
+                    <span class="news-category"><?php echo $item["sectionName"]; ?></span>
+
+                    <div class="news-date">
+                        <h1><?php echo date("d", strtotime($item["webPublicationDate"])); ?></h1>
+                        <div class="year-month">
+                            <span><?php echo date("Y", strtotime($item["webPublicationDate"])); ?></span>
+                            <span><?php echo date("M", strtotime($item["webPublicationDate"])); ?></span>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+                <?php endif; ?>  
+            </div>
+
+
+            <?php
+            $getAllSportsNews = Curl::get("https://content.guardianapis.com/search?q=sports&api-key=test");
+            $sports = $getAllSportsNews["response"]["results"];
+            ?>
+            <h1 class="section-title">Sports</h1>
+            <div class="wrapper flex-wrap">
+                <?php if(!empty($sports)): ?>
+                <?php foreach($sports as $item): ?>
+                <div class="news-item">
+                    <img src="assets/images/news/news.jpg" alt="">
+                    <h3><a target="_blank" href="<?php echo $item["webUrl"]; ?>"><?php echo $item["webTitle"]; ?></a></h3>
+                    <span class="news-category"><?php echo $item["sectionName"]; ?></span>
+
+                    <div class="news-date">
+                        <h1><?php echo date("d", strtotime($item["webPublicationDate"])); ?></h1>
+                        <div class="year-month">
+                            <span><?php echo date("Y", strtotime($item["webPublicationDate"])); ?></span>
+                            <span><?php echo date("M", strtotime($item["webPublicationDate"])); ?></span>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+                <?php endif; ?>  
+            </div>
+
+
+            <?php
+            $getAllFashionNews = Curl::get("https://content.guardianapis.com/search?q=fashion&api-key=test");
+            $fashion = $getAllFashionNews["response"]["results"];
+            ?>
+            <h1 class="section-title">Fashion</h1>
+            <div class="wrapper flex-wrap">
+                <?php if(!empty($fashion)): ?>
+                <?php foreach($fashion as $item): ?>
+                <div class="news-item">
+                    <img src="assets/images/news/news.jpg" alt="">
+                    <h3><a target="_blank" href="<?php echo $item["webUrl"]; ?>"><?php echo $item["webTitle"]; ?></a></h3>
+                    <span class="news-category"><?php echo $item["sectionName"]; ?></span>
+
+                    <div class="news-date">
+                        <h1><?php echo date("d", strtotime($item["webPublicationDate"])); ?></h1>
+                        <div class="year-month">
+                            <span><?php echo date("Y", strtotime($item["webPublicationDate"])); ?></span>
+                            <span><?php echo date("M", strtotime($item["webPublicationDate"])); ?></span>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+                <?php endif; ?>  
+            </div>
+        </div>
+    </div>
+    <!-- news end -->
+</body>
+</html>
