375387a7f52ccf6e2f315a97e83c3649cfa2aa27
Aditya-Amin <adittyaamin@gmail.com>
2020-10-23 16:02:53 +0600

    Another Debug

--- signup.php
+++ signup.php
@@ -6,23 +6,23 @@ $googleClient = $auth->googleLogin();
 // authenticate code from Google OAuth Flow
 if ( isset($_GET['code']) ) {
     session_start();
-    $token = $googleClient->fetchAccessTokenWithAuthCode( $_GET['code'] );
-    $googleClient->setAccessToken( $token['access_token'] );
-    $_SESSION['access_token'] = $token['access_token'];
+    // $token = $googleClient->fetchAccessTokenWithAuthCode( $_GET['code'] );
+    // $googleClient->setAccessToken( $token['access_token'] );
+    // $_SESSION['access_token'] = $token['access_token'];
 
-    print_r($googleClient);
+    // print_r($googleClient);
     
-    // get profile info
-    $google_oauth = new Google_Service_Oauth2($googleClient);
-    $google_account_info = $google_oauth->userinfo->get();
-    $email =  $google_account_info->email;
-    $name =  $google_account_info->name;
+    // // get profile info
+    // $google_oauth = new Google_Service_Oauth2($googleClient);
+    // $google_account_info = $google_oauth->userinfo->get();
+    // $email =  $google_account_info->email;
+    // $name =  $google_account_info->name;
 
-    print_r($google_oauth);
+    // print_r($google_oauth);
 
-    $_SESSION['name'] = $name;
-    $_SESSION['email'] = $email;
-    header( "Location: newspaper.php" );
+    // $_SESSION['name'] = $name;
+    // $_SESSION['email'] = $email;
+    // header( "Location: newspaper.php" );
 // now you can use this profile info to create account in your website and make user logged in.
 } if ( isset($_REQUEST['logout']) ){
     session_start();
