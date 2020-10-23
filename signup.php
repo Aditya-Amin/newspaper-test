f8316cd32cba2e76ae537a499edaa77226aa587d
Aditya-Amin <adittyaamin@gmail.com>
2020-10-23 14:04:07 +0600

    Login bug fixed

--- signup.php
+++ signup.php
@@ -1,5 +1,42 @@
 <?php
+require_once( dirname(__DIR__) . '/vendor/autoload.php' );
+// init configuration
+$clientID = '464996402201-jeeonc4names3jc2heupok1n31idg19f.apps.googleusercontent.com';
+$clientSecret = 'BdJ8pZyvjFIo5NVg4LbyYuBs';
+$redirectUri = 'https://glacial-fjord-18098.herokuapp.com/signup.php';
 
-require_once( 'auth/Auth.php' );
-$auth = new Auth();
-$auth->googleLogin();
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+
+// authenticate code from Google OAuth Flow
+if ( isset($_GET['code']) ) {
+    session_start();
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token['access_token']);
+
+    $_SESSION['access_token'] = $token['access_token'];
+    
+    // get profile info
+    $google_oauth = new Google_Service_Oauth2($client);
+    $google_account_info = $google_oauth->userinfo->get();
+    $email =  $google_account_info->email;
+    $name =  $google_account_info->name;
+
+    $_SESSION['name'] = $name;
+    $_SESSION['email'] = $email;
+    header( "Location newspaper.php" );
+// now you can use this profile info to create account in your website and make user logged in.
+} if ( isset($_REQUEST['logout']) ){
+    session_start();
+    unset($_SESSION['access_token']);
+    unset($_SESSION['name']);
+    unset($_SESSION['email']);
+    session_destroy();
+    $client->revokeToken();
+    header( "Location index.php" );
+}
